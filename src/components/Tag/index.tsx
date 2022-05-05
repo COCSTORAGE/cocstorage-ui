4e4d0b58e5b0ff2381b8437556a98654d3ce8b3c
kimjh96 <kimjhs@kakao.com>
2022-05-05 20:00:20 +0900

    fix: Tag 컴포넌트 스펙 수정

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, ReactElement, HTMLAttributes } from 'react';
+import React, { forwardRef, PropsWithChildren, ReactElement, HTMLAttributes } from 'react';
 import useTheme from '@theme/provider/useTheme';
 
 import { GenericComponentProps, ComponentColor } from '../../types';
@@ -6,13 +6,12 @@ import { StyledTag } from './Tag.styles';
 
 export interface TagProps extends GenericComponentProps<HTMLAttributes<HTMLDivElement>> {
   color?: ComponentColor;
-  text: string;
   startIcon?: ReactElement;
   iconOnly?: boolean;
 }
 
-const Tag = forwardRef<HTMLDivElement, TagProps>(function Tag(
-  { color = 'text', text, startIcon, iconOnly, customStyle, ...props },
+const Tag = forwardRef<HTMLDivElement, PropsWithChildren<TagProps>>(function Tag(
+  { color = 'text', children, startIcon, iconOnly, customStyle, ...props },
   ref
 ) {
   const { theme } = useTheme();
@@ -20,7 +19,7 @@ const Tag = forwardRef<HTMLDivElement, TagProps>(function Tag(
   return (
     <StyledTag ref={ref} theme={theme} tagColor={color} css={customStyle} {...props}>
       {startIcon && startIcon}
-      {!iconOnly && text}
+      {!iconOnly && children}
     </StyledTag>
   );
 });
