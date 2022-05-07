fadbd37888eb655ddeb6cc7c322e6c8e6e4dc919
kimjh96 <kimjhs@kakao.com>
2022-05-07 17:56:10 +0900

    fix: Flexbox 컴포넌트 스펙 수정

--- src/components/Flexbox/index.tsx
+++ src/components/Flexbox/index.tsx
@@ -4,7 +4,7 @@ import { GenericComponentProps } from '../../types';
 import { StyledFlexbox } from './Flexbox.styles';
 
 export interface FlexboxProps extends GenericComponentProps<HTMLAttributes<HTMLDivElement>> {
-  component?: Extract<ElementType, 'div' | 'section' | 'main' | 'article'>;
+  component?: Extract<ElementType, 'div' | 'section' | 'main' | 'article' | 'button'>;
   direction?: 'horizontal' | 'vertical';
   alignment?: 'flex-start' | 'flex-end' | 'center';
   justifyContent?: 'center' | 'space-between';
