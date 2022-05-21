3549c4161d9458258b798fda64fa35918db729e5
kimjh96 <kimjhs@kakao.com>
2022-05-21 22:01:56 +0900

    fix: GlobalStyles 컴포넌트가 ThemeProvider 에 포함되어 렌더링 되도록 수정

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,2 @@
 export { default as ThemeProvider } from './provider/ThemeProvider';
 export { default as useTheme } from './provider/useTheme';
-export { default as GlobalStyles } from './utils/GlobalStyles';
--- src/theme/provider/ThemeProvider.tsx
+++ src/theme/provider/ThemeProvider.tsx
@@ -4,18 +4,26 @@ import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
 import { dark } from '@theme/dark';
 import { light } from '@theme/light';
 
+import GlobalStyles from '@theme/utils/GlobalStyles';
+
 import { ThemeType } from '../../types';
 import ThemeContext from './ThemeContext';
 
 export interface ThemeProviderProps {
   theme: ThemeType;
+  globalStyles?: boolean;
 }
 
-function ThemeProvider({ children, theme }: PropsWithChildren<ThemeProviderProps>) {
+function ThemeProvider({
+  children,
+  theme,
+  globalStyles = true
+}: PropsWithChildren<ThemeProviderProps>) {
   const cocTheme = useMemo(() => (theme === 'light' ? light : dark), [theme]);
 
   return (
     <ThemeContext.Provider value={theme}>
+      {globalStyles && <GlobalStyles />}
       <EmotionThemeProvider theme={cocTheme}>{children}</EmotionThemeProvider>
     </ThemeContext.Provider>
   );
