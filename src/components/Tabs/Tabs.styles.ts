c4ddca4cb216ff23b71d668be2ce41f89f2c34e7
kimjh96 <kimjhs@kakao.com>
2023-03-01 15:48:26 +0900

    feat: Tabs 컴포넌트 hideLine props 추가

--- src/components/Tabs/Tabs.styles.ts
+++ src/components/Tabs/Tabs.styles.ts
@@ -2,8 +2,20 @@ import styled, { CSSObject } from '@emotion/styled';
 
 import { TabsProps } from '.';
 
-export const StyledTabs = styled.div<Pick<TabsProps, 'centered'>>`
+export const StyledTabs = styled.div<Pick<TabsProps, 'centered' | 'hideLine'>>`
   display: flex;
+
+  ${({
+    theme: {
+      palette: { box }
+    },
+    hideLine
+  }): CSSObject =>
+    !hideLine
+      ? {
+          borderBottom: `1px solid ${box.stroked.normal}`
+        }
+      : {}};
 
   ${({ centered }): CSSObject =>
     centered
