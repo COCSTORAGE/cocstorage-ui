432eb23f6b7756f76ac6bcdd4a71b4d73d9a661b
kimjh96 <kimjhs@kakao.com>
2023-03-27 23:55:30 +0900

    feat: types, utils 추가 export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export * from './components';
 export * from './theme';
-export { CustomStyle, CSSValue, ThemeMode } from './types';
+export {
+  CustomStyle,
+  CSSValue,
+  ThemeMode,
+  Color,
+  BrandColor,
+  GenericComponentProps
+} from './types';
+export { getBrandColorCode, convertNumberToCSSValue } from './utils';
